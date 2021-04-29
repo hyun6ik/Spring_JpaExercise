1350b4d8cae4ef1319eed9a5cc969a6955552d68
DESKTOP-NQ23D5Q\Hyunsik <fkanwm@phpschool.com>
2021-04-29 15:34:04 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/jpa_fast/JpaFastApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_fast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaFastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaFastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpa_fast/JpaFastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_fast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaFastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
